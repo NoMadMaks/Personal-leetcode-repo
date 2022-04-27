c0e65e1745ebb13ebf763fd01c3197455ffb3844
Maksim <maksim.borkovic@gmail.com>
2022-04-27 18:01:02 +0200

    max subarray Kadanes algorith

---
+++ Maximum_subarray(Kadane_algorithm).py
@@ -0,0 +1,8 @@
+def maxSubArray(nums):
+        for i in range(1,len(nums)):
+            nums[i] = max(nums[i], nums[i-1] + nums[i])
+        return max(nums)
+
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+x = maxSubArray(nums)
+print(x)
